eb077fd632ea69fda516fa6ac47896c945c13eea
Phương Anh <anhlhpd00579@fpt.edu.vn>
2018-10-25 19:47:06 +0700

    TEST UWP

---
+++ WindowStoreApplication/WindowStoreApplication/Views/AddPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WindowStoreApplication.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AddPage : Page
+    {
+        public AddPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            StorageFile file =
+                await folder.CreateFileAsync(this.Name.Text,
+                    CreationCollisionOption.OpenIfExists);
+            if (await ApplicationData.Current.LocalFolder.TryGetItemAsync(this.Name.Text) == null)
+            {
+                await FileIO.WriteTextAsync(file, this.Content.Text);
+            }
+            else
+            {
+                var list = new List<string>();
+                list.Add(this.Content.Text);
+                await FileIO.AppendLinesAsync(file, list);
+            }
+        }
+    }
+}
---
+++ WindowStoreApplication/WindowStoreApplication/Views/ViewPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WindowStoreApplication.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ViewPage : Page
+    {
+        public ViewPage()
+        {
+            this.InitializeComponent();
+            Page_Load();
+        }
+        
+        private async void Page_Load()
+        {
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            var files = await folder.GetFilesAsync();
+            IReadOnlyList<StorageFile> fileList = await folder.GetFilesAsync();
+            foreach (StorageFile file in fileList)
+            {
+                Debug.WriteLine(file.Name + "\n");
+            }
+            Debug.WriteLine(files);
+        }
+    }
+}
